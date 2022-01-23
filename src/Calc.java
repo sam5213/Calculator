5e460edf78e7cadb8d8708505fddadc22e5eca31
zhgb <gulmirazhb@gmail.com>
2022-01-23 18:43:13 +0300

    moved to a separate function of validation length of entered expression

--- src/Calc.java
+++ src/Calc.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Calc {
     public static final int OPERAND_COUNT = 3;
+    public static Operation operations;
 
     public static Number getValidNumber(String number) throws Exception {
         if (RimOperation.isNumeric(number)) {
@@ -13,20 +14,30 @@ public class Calc {
         } else throw new Exception("Калькулятор принимает на вход арабские и римские числа");
     }
 
+    public static boolean isExpressionLengthValid(int length) throws Exception {
+        if (length < OPERAND_COUNT) {
+            throw new Exception("строка не является математической операцией");
+        }
+
+        if (length > OPERAND_COUNT) {
+            throw new Exception("формат математической операции не удовлетворяет заданию - " +
+                    "два операнда и один оператор (+, -, /, *) ");
+        }
+
+        return length == OPERAND_COUNT;
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String expression = scanner.nextLine();
-
         String[] membersExpression = expression.split("\\s+");
-        Operation operations;
 
         try {
-            if (membersExpression.length == OPERAND_COUNT) {
+            Number number1 = getValidNumber(membersExpression[0]);
+            Number number2 = getValidNumber(membersExpression[2]);
 
-                Number number1 = getValidNumber(membersExpression[0]);
-                Number number2 = getValidNumber(membersExpression[2]);
-
+            if (isExpressionLengthValid(membersExpression.length)) {
                 if (number1.getNotationValue() == Notation.ARABIC && number2.getNotationValue() == Notation.ARABIC) {
                     operations = new ArabicOperation(number1, number2);
 
@@ -39,11 +50,6 @@ public class Calc {
                 String result = operations.calculate(membersExpression[1]);
                 System.out.println(result);
 
-            } else if (membersExpression.length > OPERAND_COUNT) {
-                throw new Exception("формат математической операции не удовлетворяет заданию - " +
-                        "два операнда и один оператор (+, -, /, *) ");
-            } else {
-                throw new Exception("строка не является математической операцией");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
